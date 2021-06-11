6aa151a861acb1763dcab070ed8cce1f4242716b
Green <greenakawushim@gmail.com>
2021-06-11 23:37:48 +0100

    adding missing terminations

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const table = document.querySelector('table');
 const button = document.querySelector('.buttons')
 const clearTable = document.querySelector('.clear-table');
 const calculate = document.querySelector('.calculate-gp');
-const result = document.querySelector('.result')
-const body = document.querySelector('body')
-const sectionForm = document.querySelector('.section-form')
-const resultAfter = document.querySelector('.result-after')
-let unitList = []
-let gradeList = []
+const result = document.querySelector('.result');
+const body = document.querySelector('body');
+const sectionForm = document.querySelector('.section-form');
+const resultAfter = document.querySelector('.result-after');
+const loading = document.querySelector('.loading');
+let unitList = [];
+let gradeList = [];
 
 
 //Total Units function
@@ -26,32 +27,32 @@ function calculateUnits(){
 
 //prompt message function
 function message(text, classname){
-  let p = document.createElement('p')
-  p.className = classname
-  p.innerHTML = text
+  let p = document.createElement('p');
+  p.className = classname;
+  p.innerHTML = text;
   return p
 }
 
 //Units * Grades function
 function calculateGrades(){
-  var sum_grades = 0
+  var sum_grades = 0;
   for(i = 0; i < unitList.length; i++){
     for(j = i; j <= i; i++) {
-      sum_grades += unitList[i] * gradeList[j]
+      sum_grades += unitList[i] * gradeList[j];
       break;
-    }
-  }
-  return sum_grades
+    };
+  };
+  return sum_grades;
 }
 
 
 
 //Create and add items to table
-form.addEventListener('submit', addItem)
+form.addEventListener('submit', addItem);
 
 function addItem(e) {
   if(course_code.value === '' || unit.value === '' || grade.value === ''){
-    alert('please fill all fields')
+    alert('please fill all fields');
   }else {
     let tr = document.createElement('tr');
     tr.innerHTML = `<td>${course_code.options[course_code.selectedIndex].text}</td>
@@ -60,21 +61,22 @@ function addItem(e) {
                     <td>${grade.options[grade.selectedIndex].value * unit.value}</td>
                     <td><a class="remove-item"><i class="ion-close-round"></i></a></td>
                     `
-    table.appendChild(tr)
-    button.style.display = "block"
-    unitList.push(Number(unit.value))
-    gradeList.push(Number(grade.options[grade.selectedIndex].value))
+    table.appendChild(tr);
+    button.style.display = "block";
+    loading.style.display = "block";
+    unitList.push(Number(unit.value));
+    gradeList.push(Number(grade.options[grade.selectedIndex].value));
 
-    course_code.value = ''
-    unit.value = ''
-    grade.value = ''
+    course_code.value = '';
+    unit.value = '';
+    grade.value = '';
 
-    let paragraph = message('Items added successfully', 'success')
-    sectionForm.insertBefore(paragraph, form)
+    let paragraph = message('Items added successfully', 'success');
+    sectionForm.insertBefore(paragraph, form);
 
     setTimeout(function(){
       (sectionForm.insertBefore(paragraph, form)).remove();
-    }, 2000) 
+    }, 2000);
 
     e.preventDefault();
 
@@ -83,41 +85,43 @@ function addItem(e) {
 };
 
 //Remove individual rows in table
-table.addEventListener('click', removeItem)
+table.addEventListener('click', removeItem);
 
 function removeItem(e){
   if (e.target.className == 'ion-close-round'){
-    e.target.parentElement.parentElement.parentElement.remove()
+    e.target.parentElement.parentElement.parentElement.remove();
   };
 };
 
 
 //Remove all items in the table(clear button)
-clearTable.addEventListener('click', removeAllItems)
+clearTable.addEventListener('click', removeAllItems);
 
 function removeAllItems(){
-  let td = document.querySelectorAll('td')
+  let td = document.querySelectorAll('td');
   td.forEach(function(item){
-    item.style.display = 'none'
-  })
-  button.style.display = "none"
-  result.style.display = "none"
+    item.style.display = 'none';
+  });
+  button.style.display = "none";
+  result.style.display = "none";
 }
 
 
 //Calculate GP(calculate button)
-calculate.addEventListener('click', calculateGP)
+calculate.addEventListener('click', calculateGP);
 
 function calculateGP(){
-  let tr = document.createElement('tr')
+
+  
+  let tr = document.createElement('tr');
   tr.innerHTML = `<td>Total</td>
                   <td>${calculateUnits()}</td>
                   <td></td>
                   <td>${calculateGrades()}</td>
                   `
-  table.appendChild(tr)
+  table.appendChild(tr);
 
-  result.innerHTML = `Your GPA is ${(calculateGrades() / calculateUnits()).toFixed(2)}`
+  result.innerHTML = `Your GPA is ${(calculateGrades() / calculateUnits()).toFixed(2)}`;
   
 }
 
