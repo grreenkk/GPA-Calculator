6146d88d2b2bb4317d3b99fdd475dff4613c249e
Green <greenakawushim@gmail.com>
2021-06-12 10:26:46 +0100

    general modifications

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Variable Selectors
 const form = document.querySelector('form');
 const course_code = document.getElementById('course_code');
 const unit = document.getElementById('unit');
@@ -6,53 +7,58 @@ const table = document.querySelector('table');
 const button = document.querySelector('.buttons')
 const clearTable = document.querySelector('.clear-table');
 const calculate = document.querySelector('.calculate-gp');
-const result = document.querySelector('.result')
-const body = document.querySelector('body')
-const sectionForm = document.querySelector('.section-form')
-const resultAfter = document.querySelector('.result-after')
-let unitList = []
-let gradeList = []
+const result = document.querySelector('.result');
+const body = document.querySelector('body');
+const sectionForm = document.querySelector('.section-form');
+const resultAfter = document.querySelector('.result-after');
+loading = document.querySelector('.loading');
+let unitList = [];
+let gradeList = [];
 
 
-
+//Total Units Function
 function calculateUnits(){
 
   var sum = 0;
   for(var i = 0; i < unitList.length; i++){
      sum += unitList[i];
-  }
-  
-  return sum;
+  };
+  return sum; 
+};
 
-  
-}
-
+//Prompt message Function
 function message(text, classname){
-  let p = document.createElement('p')
-  p.className = classname
-  p.innerHTML = text
-  return p
+  let p = document.createElement('p');
+  p.className = classname;
+  p.innerHTML = text;
+  return p;
 }
 
+//Sub Total function
 function calculateGrades(){
-  var sum_grades = 0
+  var sum_grades = 0;
   for(i = 0; i < unitList.length; i++){
     for(j = i; j <= i; i++) {
-      sum_grades += unitList[i] * gradeList[j]
+      sum_grades += unitList[i] * gradeList[j];
       break;
-    }
-  }
-  return sum_grades
-}
+    };
+  };
+  return sum_grades;
+};
 
 
 
-
+//Create and add items to table
 form.addEventListener('submit', addItem)
 
 function addItem(e) {
   if(course_code.value === '' || unit.value === '' || grade.value === ''){
-    alert('please fill all fields')
+    let paragraph = message('Please fill all fields', 'error');
+    sectionForm.insertBefore(paragraph, form);
+  
+    setTimeout(function(){
+      (sectionForm.insertBefore(paragraph, form)).remove();
+    }, 2000); 
   }else {
     let tr = document.createElement('tr');
     tr.innerHTML = `<td>${course_code.options[course_code.selectedIndex].text}</td>
@@ -61,80 +67,89 @@ function addItem(e) {
                     <td>${grade.options[grade.selectedIndex].value * unit.value}</td>
                     <td><a class="remove-item"><i class="ion-close-round"></i></a></td>
                     `
-    table.appendChild(tr)
-    button.style.display = "block"
-    unitList.push(Number(unit.value))
-    gradeList.push(Number(grade.options[grade.selectedIndex].value))
-    // sum_array = units(parseInt(unit.value));
-    // console.log(units(Number(unit.value)))
-    console.log(gradeList)
+    table.appendChild(tr);
+    button.style.display = "block";
+    unitList.push(Number(unit.value));
+    gradeList.push(Number(grade.options[grade.selectedIndex].value));
+    console.log(gradeList);
 
-    course_code.value = ''
-    unit.value = ''
-    grade.value = ''
+    course_code.value = '';
+    unit.value = '';
+    grade.value = '';
 
-    let paragraph = message('Items added successfully', 'success')
-    sectionForm.insertBefore(paragraph, form)
+    let paragraph = message('Items added successfully', 'success');
+    sectionForm.insertBefore(paragraph, form);
 
     setTimeout(function(){
       (sectionForm.insertBefore(paragraph, form)).remove();
-    }, 2000) 
+    }, 2000); 
 
-    e.preventDefault();
-
-    
-  }
+  };
+  e.preventDefault();
 };
 
-table.addEventListener('click', removeItem)
+
+//Remove individual rows in table
+table.addEventListener('click', removeItem);
 
 function removeItem(e){
   if (e.target.className == 'ion-close-round'){
-    e.target.parentElement.parentElement.parentElement.remove()
+    e.target.parentElement.parentElement.parentElement.remove();
   };
   
+  let paragraph = message('Items deleted successfully', 'success');
+  sectionForm.insertBefore(paragraph, form);
 
+  setTimeout(function(){
+    (sectionForm.insertBefore(paragraph, form)).remove();
+  }, 2000); 
+
+  e.preventDefault();
 };
 
-clearTable.addEventListener('click', removeAllItems)
+
+//Remove all items in the table(clear button)
+clearTable.addEventListener('click', removeAllItems);
 
 function removeAllItems(){
   let td = document.querySelectorAll('td')
   td.forEach(function(item){
-    item.style.display = 'none'
+    item.style.display = 'none';
   })
-  button.style.display = "none"
-  result.style.display = "none"
+  button.style.display = "none";
+  result.style.display = "none";
+
+  let paragraph = message('All Items deleted successfully', 'success');
+  sectionForm.insertBefore(paragraph, form);
+
+  setTimeout(function(){
+    (sectionForm.insertBefore(paragraph, form)).remove();
+  }, 2000); 
+  
 }
 
 calculate.addEventListener('click', calculateGP)
 
+//Calculate GP(calculate button)
 function calculateGP(){
-  let tr = document.createElement('tr')
+  
+  loading.style.display = "block"
+  let tr = document.createElement('tr');
   tr.innerHTML = `<td>Total</td>
                   <td>${calculateUnits()}</td>
                   <td></td>
                   <td>${calculateGrades()}</td>
                   `
-  table.appendChild(tr)
-  // let unitss = units(unit.value)
-  // let sum_units
-  // sum_units = unitss.reduce(function(a, b){
-  //   return(a + b)
-  // }, 0);
-  
-  // console.log(unitss)
-  result.innerHTML = `Your GPA is ${(calculateGrades() / calculateUnits()).toFixed(2)}`
-  
-  
+  table.appendChild(tr);
 
-  // result.innerHTML = (calculateUnits() + calculateGrades()) / calculateUnits()
-  
+  result.innerHTML = `Your GPA is ${(calculateGrades() / calculateUnits()).toFixed(2)}`
+
+  result.style.display = "none"
+  setTimeout(function(){
+    loading.style.display = "none";
+    result.style.display = "block";
+  }, 1500); 
+
 }
 
-// var sum = 0;
-// for(var i = 0; i < unitList.length; i++){
-//      sum += unitList[i];
-// }
 
-// console.log(sum)
